29f3db23e0c0af0871aaa36447b10175b014ccc3
Leandro Oviedo <leandrog.oviedo@gmail.com>
2022-09-19 20:03:04 -0300

    Misma funcionalidad pero código más prolijo

--- main.js
+++ main.js
@@ -1,60 +1,68 @@
+//Declaro las variables principales a usar en el código.
 let supCubiertaNumero;
 let supSemiCubiertaNumero;
-
+let supTotal;
+const coefSemi = 0.5;
 let click = document.getElementById("obtener");
 let supCubierta = document.getElementById("sCubierta");
 let supSemiCubierta = document.getElementById("sSemiCubierta");
+let sGradoElec = document.getElementById("sTotal");
+let sTotal = document.getElementById("sTotal");
+let gradoElectrificacionAMostrar = document.getElementById("gradoElectrificacion")
+let mostrarCantidadCircuitos = document.getElementById("cantCircuitos");
+let gradoElec;
+let gradoElecComparar;
 
+
+//Obtengo datos desde el DOM
 obtener.onclick = () => {
-    const coefSemi = 0.5;
     supCubiertaNumero = Number(supCubierta.value);
     supSemiCubiertaNumero = Number(supSemiCubierta.value);
-    let supTotal = (supCubiertaNumero + (supSemiCubiertaNumero * coefSemi));
-    console.log(supTotal);
-    let sTotal = document.getElementById("sTotal");
-    sTotal.innerText = supTotal;
-    gelct();
+    supTotal = (supCubiertaNumero + (supSemiCubiertaNumero * coefSemi));
+    actualizarSuperficieTotal();
+    gradoElectrificacion();
+    cantidadDeCircuitosAUsar();
 };
+//Declaro la funcion para actualizar la superficie total en el DOM
+function actualizarSuperficieTotal() {
+    sTotal.innerText = supTotal;
 
-function gelct() {
-    let sgradoelec = document.getElementById("sTotal")
-    sgradoelec = Number(sgradoelec.innerHTML)
-    let gradoElec = '';
-
-    if (sgradoelec <= 60) {
-        //alert('Grado de electrificación mínimo (hasta 60m2)')
+}
+// Declaro la función para conocer el grado de electrificacion de la vivienda
+function gradoElectrificacion() {
+    sGradoElec = supTotal
+    if (sGradoElec <= 60) {
         gradoElec = 'minimo'
-    } else if (sgradoelec > 60 && sgradoelec <= 130) {
-        //alert('Grado de electrificación medio (hasta 130m2)')
+    } else if (sGradoElec > 60 && sGradoElec <= 130) {
         gradoElec = 'medio'
-    } else if (sgradoelec > 130 && sgradoelec <= 200) {
-        //alert('Grado de electrificación elevado (hasta 200m2)')
+    } else if (sGradoElec > 130 && sGradoElec <= 200) {
         gradoElec = 'elevado'
-    } else if (sgradoelec > 200) {
-        //alert('Grado de electrificación superior (más de 200m2)')
+    } else if (sGradoElec > 200) {
         gradoElec = 'superior'
     }
-    console.log(gradoElec);
-    let escri = document.getElementById("cantCircuitos")
+    gradoElectrificacionAMostrar.innerText = gradoElec;
+    
+}
 
+function cantidadDeCircuitosAUsar() {
     switch (gradoElec) {
         case gradoElec = 'minimo':
-            escri.innerText = ('Se deben usar al menos dos circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG')
+            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos dos circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG')
             break;
         case gradoElec = 'medio':
 
-            escri.innerText = ('Se deben usar al menos tres circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG' + '\n' + '1- Libre elección')
+            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos tres circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG' + '\n' + '1- Libre elección')
             break;
         case gradoElec = 'elevado':
 
-            escri.innerText = ('Se deben usar al menos cinco circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE')
+            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos cinco circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE')
             break;
         case gradoElec = 'superior':
-            escri.innerText = ('Se deben usar al menos seis circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE' + '\n' + '1- Libre elección')
+            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos seis circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE' + '\n' + '1- Libre elección')
             break;
 
         default:
-            alert('ojo estamos en el caso default')
+            mostrarCantidadCircuitos.innerText = ('ojo estamos en el caso default')
             break;
     }
 }
