1fdfe3a0b3d059a49a01e10fce283d11a53cb86d
Leandro Oviedo <leandrog.oviedo@gmail.com>
2022-09-29 19:45:36 -0300

    Arreglada la funcionalidad de la memoria

--- app.js
+++ app.js
@@ -1,156 +1,134 @@
 //Creo un array donde voy a guardar los distintos ambientes de la vivienda.
-const rooms = [];
+const rooms = JSON.parse(localStorage.getItem("room")) || [];
 let roomId = 0;
-let roomName1;
-let roomCSurface;
-let roomScsurface;
 let totalCoveredSurface;
 let totalSemicoveredSurface;
 let totalSurface;
 let gradoElec;
-let sGradoElec = document.getElementById("sTotal");
-let gradoElectrificacionAMostrar = document.getElementById("gradoElectrificacion")
+let gradoElectrificacionAMostrar = document.getElementById("gradoElectrificacion");
 let mostrarCantidadCircuitos = document.getElementById("cantCircuitos");
-const calculate = document.getElementById('calculate');
-const saveRoom = document.getElementById('saveRooms')
-const getRoom = document.getElementById('getRooms')
-
+const calculate = document.getElementById("calculate");
+const saveRoom = document.getElementById("saveRooms");
+const recuperar = document.getElementById("getRooms");
+let recuperarState = false;
 
 //Leo desde el html los datos a ingresar al array de ambientes y los pusheo.
-const createRooms = () => {
-    roomId ++ ;
-    const roomName = document.getElementById('roomName');
-    const roomCoveredSurface = document.getElementById('roomCoveredSurface');
-    const roomSemicoveredSurface = document.getElementById('roomSemicoveredSurface');
-    roomName1 = roomName.value;
-    roomCSurface = parseFloat(roomCoveredSurface.value);
-    roomScsurface = parseFloat(roomSemicoveredSurface.value);
+saveRoom.addEventListener("click", () => {
+    roomId++;
+    const roomName = document.getElementById("roomName").value;
+    const roomCoveredSurface = parseFloat(document.getElementById("roomCoveredSurface").value);
+    const roomSemicoveredSurface = parseFloat(document.getElementById("roomSemicoveredSurface").value);
     rooms.push({
         roomId,
-        roomName1,
-        roomCSurface,
-        roomScsurface
+        roomName,
+        roomCoveredSurface,
+        roomSemicoveredSurface,
     });
-    saveRoomStorage(rooms);
-}
+    localStorage.setItem("room", JSON.stringify(rooms));
+    roomName.value = "";
+    roomCoveredSurface.value = "0";
+    roomSemicoveredSurface.value = "0";
+
+    recuperarState = true;
+    
+    renderRooms();
+});
 
 const getTotalSurface = () => {
-    totalCoveredSurface = rooms.reduce((acc, item) => acc + item.roomCSurface, 0);
-    totalSemicoveredSurface = rooms.reduce((acc, item) => acc + item.roomScsurface, 0);
-    totalSurface = (totalCoveredSurface + (totalSemicoveredSurface * 0.5))
+    totalCoveredSurface = rooms.reduce((acc, item) => acc + item.roomCoveredSurface, 0);
+    totalSemicoveredSurface = rooms.reduce((acc, item) => acc + item.roomSemicoveredSurface, 0);
+    totalSurface = totalCoveredSurface + totalSemicoveredSurface * 0.5;
     sTotal.innerText = totalSurface;
-}
+};
 
 const renderRooms = () => {
-    const tableRooms = document.getElementById('roomsTable');
-    let tableBody = document.createElement('tbody');
+    const tableRooms = document.getElementById("roomsTable");
+    let tableBody = document.createElement("tbody");
 
-    rooms.forEach(room => {
-        tableBody.innerHTML = "";
+    rooms.forEach((room) => {
+        if (recuperarState) {
+            tableBody.innerHTML = "";
+        }
+
+        let row = document.createElement("tr");
+        let td = document.createElement("td");
         
-        let row = document.createElement('tr');
-
-        let td = document.createElement('td');
-        td.innerText = room.roomName1;
+        td.innerText = room.roomName;
         row.appendChild(td);
 
-        td = document.createElement('td');
-        td.innerText = room.roomCSurface;
+        td = document.createElement("td");
+        td.innerText = room.roomCoveredSurface;
         row.appendChild(td);
 
-        td = document.createElement('td');
-        td.innerText = room.roomScsurface;
+        td = document.createElement("td");
+        td.innerText = room.roomSemicoveredSurface;
         row.appendChild(td);
 
-        td = document.createElement('td');
+        td = document.createElement("td");
         td.innerHTML = `<button id="${room.roomId}" class="btn btn-danger">x</button>`;
         row.appendChild(td);
 
         tableBody.appendChild(row);
-    })
-    tableRooms.appendChild(tableBody);
-
-    tableRooms.addEventListener('click', (e) => {
-        eraseRoom(e.target.roomId);
     });
+    tableRooms.appendChild(tableBody);
 };
 
 // Declaro la función para conocer el grado de electrificacion de la vivienda
 function gradoElectrificacion() {
-    sGradoElec = totalSurface
-    if (sGradoElec <= 60) {
-        gradoElec = 'minimo'
-    } else if (sGradoElec > 60 && sGradoElec <= 130) {
-        gradoElec = 'medio'
-    } else if (sGradoElec > 130 && sGradoElec <= 200) {
-        gradoElec = 'elevado'
-    } else if (sGradoElec > 200) {
-        gradoElec = 'superior'
+    if (totalSurface <= 60) {
+        gradoElec = "minimo";
+    } else if (totalSurface > 60 && totalSurface <= 130) {
+        gradoElec = "medio";
+    } else if (totalSurface > 130 && totalSurface <= 200) {
+        gradoElec = "elevado";
+    } else if (totalSurface > 200) {
+        gradoElec = "superior";
     }
     gradoElectrificacionAMostrar.innerText = gradoElec;
 }
 
 function cantidadDeCircuitosAUsar() {
     switch (gradoElec) {
-        case gradoElec = 'minimo':
-            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos dos circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG')
+        case (gradoElec = "minimo"):
+            mostrarCantidadCircuitos.innerText = "Se deben usar al menos dos circuitos: " + "\n" + "1- IUG" + "\n" + "1- TUG";
             break;
-        case gradoElec = 'medio':
-
-            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos tres circuitos: ' + '\n' + '1- IUG' + '\n' + '1- TUG' + '\n' + '1- Libre elección')
+        case (gradoElec = "medio"):
+            mostrarCantidadCircuitos.innerText =
+                "Se deben usar al menos tres circuitos: " + "\n" + "1- IUG" + "\n" + "1- TUG" + "\n" + "1- Libre elección";
             break;
-        case gradoElec = 'elevado':
-
-            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos cinco circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE')
+        case (gradoElec = "elevado"):
+            mostrarCantidadCircuitos.innerText =
+                "Se deben usar al menos cinco circuitos: " + "\n" + "2- IUG" + "\n" + "2- TUG" + "\n" + "1- TUE";
             break;
-        case gradoElec = 'superior':
-            mostrarCantidadCircuitos.innerText = ('Se deben usar al menos seis circuitos: ' + '\n' + '2- IUG' + '\n' + '2- TUG' + '\n' + '1- TUE' + '\n' + '1- Libre elección')
+        case (gradoElec = "superior"):
+            mostrarCantidadCircuitos.innerText =
+                "Se deben usar al menos seis circuitos: " +
+                "\n" +
+                "2- IUG" +
+                "\n" +
+                "2- TUG" +
+                "\n" +
+                "1- TUE" +
+                "\n" +
+                "1- Libre elección";
             break;
 
         default:
-            mostrarCantidadCircuitos.innerText = ('ojo estamos en el caso default')
+            mostrarCantidadCircuitos.innerText = "ojo estamos en el caso default";
             break;
     }
 }
 
-const saveRoomStorage = (rooms) => {
-    localStorage.setItem('room', JSON.stringify(rooms));
-};
+recuperar.addEventListener("click", renderRooms);
 
-const retriveRooms = () =>{
-    const storedRooms = JSON.parse(localStorage.getItem('rooms'));
-    return storedRooms;
-}
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('rooms')) {
-        rooms = retriveRooms();
-        renderRooms(rooms);
-    }
-    console.log(rooms);
-})
-
-
-const eraseRoom = (roomId) =>{
-    console.log(rooms.roomId);
-    rooms.forEach((room,id)=>{
-        if(room.roomId === roomId){
-            rooms.splice(id,1);
-        }
-    })
-
-}
-
-roomsForm.addEventListener('submit', (e) => {
+roomsForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    createRooms();
-    renderRooms();
-
-    document.getElementById('roomsForm').reset();
+    /* createRooms();
+    renderRooms(); */
 });
-
 
 calculate.onclick = () => {
     getTotalSurface();
     gradoElectrificacion();
     cantidadDeCircuitosAUsar();
-}
+};
